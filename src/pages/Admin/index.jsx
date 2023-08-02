90cf4220d732d9377a505febcebcb20c66267dd4
Reiser9 <63304397+Reiser9@users.noreply.github.com>
2023-08-02 19:43:36 +0500

    test dvh in modal

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -11,7 +11,9 @@ import WithSidebarWrapper from '../../components/Wrapper/WithSidebarWrapper';
 import SidebarLink from '../../components/SidebarLink';
 
 const AdminMain = React.lazy(() => import("./subpages/AdminMain"));
+const Notifies = React.lazy(() => import("./subpages/Notifies"));
 const Users = React.lazy(() => import("./subpages/Users"));
+const Appearance = React.lazy(() => import("./subpages/Appearance"));
 
 const Admin = () => {
     return (
@@ -22,16 +24,18 @@ const Admin = () => {
                         <div className={pws.content}>
                             <Routes>
                                 <Route index element={withSuspense(<AdminMain />)} />
+                                <Route path="/notifies" element={withSuspense(<Notifies />)} />
                                 <Route path="/users" element={withSuspense(<Users />)} />
+                                <Route path="/appearance" element={withSuspense(<Appearance />)} />
                                 <Route path="*" element={<Navigate to={""} />} />
                             </Routes>
                         </div>
 
                         <div className={pws.sidebar}>
                             <SidebarLink text="Главная" to="" end />
-                            <SidebarLink text="Уведомления" to="notify" />
+                            <SidebarLink text="Уведомления" to="notifies" />
                             <SidebarLink text="Пользователи" to="users" />
-                            <SidebarLink text="Внешний вид" to="view" />
+                            <SidebarLink text="Внешний вид" to="appearance" />
                         </div>
                     </div>
                 </WithSidebarWrapper>
---
+++ src/pages/Admin/subpages/Notifies.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from '../index.module.css';
+
+const Notifies = () => {
+    return (
+        <div>Уведомления</div>
+    )
+}
+
+export default Notifies;
